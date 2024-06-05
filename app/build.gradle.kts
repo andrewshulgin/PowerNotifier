f6fadf8f5645c937d4dc887f7b0b0cb441641601
Andrew Shulgin <andrewshulginua@gmail.com>
2024-06-05 15:04:25 +0300

    release 0.0.2: implemented http retry logic; show last response in ui; clear last state on disable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.hex128.powernotifier"
         minSdk = 14
         targetSdk = 34
-        versionCode = 202406040
-        versionName = "0.0.1"
+        versionCode = 202406050
+        versionName = "0.0.2"
     }
 
     buildTypes {
