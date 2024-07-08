b5f5b1063d5b992e1180d3d8da484ef306caa5d8
Andrew Shulgin <andrewshulginua@gmail.com>
2024-07-08 14:19:39 +0300

    release 0.1.1: fixed crash in case of empty custom urls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.hex128.powernotifier"
         minSdk = 14
         targetSdk = 34
-        versionCode = 202407080
-        versionName = "0.1.0"
+        versionCode = 202407120
+        versionName = "0.1.1"
     }
 
     buildTypes {
